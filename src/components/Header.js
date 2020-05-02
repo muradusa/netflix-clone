4d3d96b6aad4fefc55466d49cf244ad1f0643d11
Murad Cholukov <muradusa@gmail.com>
2020-05-02 13:58:03 -0700

    added sign in button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import logo from "../svg/logo.svg";
+import { NavLink } from "react-router-dom";
+import "../css/Header.css"
+
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                <h1>Header</h1>
+            <div className="header-container">
+                <div className="header-top">
+                    <img src={logo} alt="Netflix logo goes here"/>
+                    <NavLink to="/" className="signIn-btn">Sign In</NavLink>
+                </div>
             </div>
         );
     }
 }
 
 export default Header;
+
