d6899d346cee677e9afdc4148389ec53ed064cbd
Murad Cholukov <muradusa@gmail.com>
2020-05-03 09:08:06 -0700

    added styles to Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import logo from "../svg/logo.svg";
 import { NavLink } from "react-router-dom";
-import "../css/Header.css"
-
+import styled from "styled-components";
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header-container">
-                <div className="header-top">
-                    <img src={logo} alt="Netflix logo goes here"/>
-                    <NavLink to="/" className="signIn-btn">Sign In</NavLink>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <HeaderComponent className="header-container">
+        <div className="header-top">
+          <Logo src={logo} alt="Netflix logo goes here" />
+          <NavLink to="/" className="signIn-btn">
+            Sign In
+          </NavLink>
+        </div>
+        {/* Header Content */}
+        <div className="header-content">
+            
+        </div>
+      </HeaderComponent>
+    );
+  }
 }
 
 export default Header;
 
+//Logo
+const Logo = styled.img`
+  width: 10 rem;
+  height: 3.5 rem;
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+// Header Container
+const HeaderComponent = styled.div`
+  .signIn-btn {
+    right: 0;
+    margin: 1.125rem 3% 0;
+    padding: 0.4375rem 1.0625rem;
+    font-weight: 400;
+    line-height: getDefaultNormalizer;
+    border-radius: 0.1875rem;
+    font-size: 1rem;
+    background: var(--main-red);
+    position: absolute;
+    translate: TransformStream(-50%, -50%);
+    cursor: pointer;
+    transition: background 0.2s ease-in;
+    &:hover {
+      background: var(--main-red-hover);
+    }
+  }
+
+  //Header Top
+  .header-top {
+      position: relative;
+      height: 10rem;
+      z-index: 1;
+  }
+`;
