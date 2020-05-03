18729a678b258be2bdb5d8e0a379ec142e025eb8
Murad Cholukov <muradusa@gmail.com>
2020-05-03 10:25:08 -0700

    Finished Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import logo from "../svg/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import styled from "styled-components";
+import { Icon } from "react-icons-kit";
+import { ic_navigate_next } from "react-icons-kit/md/ic_navigate_next";
 
 class Header extends Component {
   render() {
@@ -15,7 +17,12 @@ class Header extends Component {
         </div>
         {/* Header Content */}
         <div className="header-content">
-            
+          <Title>See what's next.</Title>
+          <Subtitle>WATCH ANYWHERE. CANCEL ANYTIME.</Subtitle>
+          <Link to="/" className="main-offer-btn">
+            try it now
+            <Icon className="Icon" icon={ic_navigate_next} size={37} />
+          </Link>
         </div>
       </HeaderComponent>
     );
@@ -26,8 +33,8 @@ export default Header;
 
 //Logo
 const Logo = styled.img`
-  width: 10 rem;
-  height: 3.5 rem;
+  width: 10rem;
+  height: 3.5rem;
   position: absolute;
   top: 25%;
   left: 50%;
@@ -56,8 +63,66 @@ const HeaderComponent = styled.div`
 
   //Header Top
   .header-top {
-      position: relative;
-      height: 10rem;
-      z-index: 1;
+    position: relative;
+    height: 10rem;
+    z-index: 1;
   }
+
+  // Header Content
+  .header-content {
+    width: 65%;
+    position: relative;
+    margin: 4.5rem auto 0;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+
+  // Main Offer Btn
+  .main-offer-btn {
+    display: inline-block;
+    background: var(--main-red);
+    text-transform: uppercase;
+    border: none;
+    outline: none;
+    margin: 0 33%;
+    padding: 1.5rem;
+    border-radius: 0.1875rem;
+    font-size: 2rem;
+    text-align: center;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+    transition: background 0.2s ease-in;
+    cursor: pointer;
+    &:hover {
+      background: var(--main-red-hover);
+    }
+
+    .Icon svg {
+      vertical-align: bottom;
+      margin-left: 1.5rem;
+    }
+
+  }
+`;
+
+// Main Title
+
+const Title = styled.h1`
+  margin: 0 0 1.2rem;
+  font-size: 5rem;
+  font-weight: 700;
+  line-height: 1.1em;
+`;
+
+// Subtitle
+
+const Subtitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.875rem;
+  line-height: 1.25rem;
+  margin: 0 0 1.875rem;
+  text-transform: uppercase;
 `;
