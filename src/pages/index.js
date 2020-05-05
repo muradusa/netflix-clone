9c531fda726c12f9bc36e04e3d2ca1e6e0f1a013
Murad Cholukov <muradusa@gmail.com>
2020-05-05 13:33:19 -0700

    started with Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from "../components/Header";
 import TabComponent from "../components/TabComponent";
+import Footer from "../components/Footer";
 
 
 class Main extends Component {
@@ -9,6 +10,7 @@ class Main extends Component {
             <div>
                 <Header />
                 <TabComponent />
+                <Footer />
                 
             </div>
         );
