f439a190b4176c93a7529522048285d3eacc54af
Murad Cholukov <muradusa@gmail.com>
2020-05-13 20:10:51 -0700

    Sign In page design complete

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,75 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import FBlogo from "../../images/fb-logo.png";
 
 class LoginForm extends Component {
-    render() {
-        return (
-            <FormContainer>
-                <div className="form-container">
-                    <form> 
-                        <h1>Sign In</h1>
-                        <div className="input-container">
-                            <input className="input-empty" type="email" required />
-                            <label >Email or Phone Number</label>
-                        </div>
-                        <div className="input-container">
-                            <input className="input-empty" type="password" required />
-                            <label>Password</label>
-                        </div>
-                        <div className="input-container">
-                            <Button type="submit">Sign In</Button>
-                        </div>
-                        <label className="checkbox-container">
-                            Remember me 
-                            <input type="checkbox" checked />
-                            <span className="checkmark"></span>
-                        </label>
-                        <Link to="/" className="need-help">
-                            Need help?
-                        </Link>
-                    </form>
-                </div>
-            </FormContainer>
-        );
-    }
+  render() {
+    return (
+      <FormContainer>
+        <div className="form-container">
+          <form>
+            <h1>Sign In</h1>
+            <div className="input-container">
+              <input className="input-empty" type="email" required />
+              <label>Email or Phone Number</label>
+            </div>
+            <div className="input-container">
+              <input className="input-empty" type="password" required />
+              <label>Password</label>
+            </div>
+            <div className="input-container">
+              <Button type="submit">Sign In</Button>
+            </div>
+            <label className="checkbox-container">
+              Remember me
+              <input type="checkbox" checked />
+              <span className="checkmark"></span>
+            </label>
+            <Link to="/" className="need-help">
+              Need help?
+            </Link>
+            <div className="bottom-form">
+              <img src={FBlogo} alt="Facebook Logo" />
+              <Link to="/" className="login-fb-text">
+                Login with Facebook
+              </Link>
+              <br/>
+              <br/>
+              <span style={{ color: '#999'}}>New to Netflix</span>&nbsp;
+              <Link to="/" className="sign-up-text">
+                  Sign up now
+              </Link>
+            </div>
+          </form>
+        </div>
+      </FormContainer>
+    );
+  }
 }
 
 export default LoginForm;
 
 //Form Container
 
-const FormContainer = styled.div` 
+const FormContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  position: relative;
+  z-index: 5;
 
-display: grid;
-justify-content: center;
-position: relative;
-z-index: 5;
-
-.form-container {
-    background: rgba(0,0,0,0.8);
+  .form-container {
+    background: rgba(0, 0, 0, 0.8);
     position: relative;
     width: 28.125rem;
     height: 41.25rem;
     padding: 4rem;
-}
+  }
 
-.input-container {
+  .input-container {
     display: grid;
     grid-template-columns: 1fr;
     margin-top: 1.2rem;
-}
+  }
 
-.input-empty {
+  .input-empty {
     color: #fff;
     background: #333;
     border: 0;
     border-radius: 0.25rem;
     height: 3rem;
     padding: 0.9rem 1.25rem 0;
-}
+  }
 
-form div label {
+  form div label {
     position: absolute;
     top: 0.625rem;
     left: 1.25ren;
@@ -77,36 +89,36 @@ form div label {
     color: #8a8a8a;
     font-size: 1rem;
     transition: transform 150ms ease-out, font-size 150ms ease-out;
-}
+  }
 
-form div {
+  form div {
     position: relative;
-}
+  }
 
-input:focus ~ label {
+  input:focus ~ label {
     top: 0.4375rem;
     font-size: 0.7rem;
-}
+  }
 
-input:focus {
+  input:focus {
     outline: none;
-}
+  }
 
-//Checkbox
-.checkbox-container {
+  //Checkbox
+  .checkbox-container {
     margin-left: 0.75rem;
     padding-left: 1.875rem;
     position: relative;
     font-size: 0.9rem;
+    color: #999;
     cursor: pointer;
+  }
 
-}
-
-.checkbox-container input {
+  .checkbox-container input {
     display: none;
-}
+  }
 
-.checkbox-container .checkmark {
+  .checkbox-container .checkmark {
     display: inline-block;
     background: #454545;
     width: 1.1rem;
@@ -115,9 +127,9 @@ input:focus {
     top: 0;
     border-radius: 0.1rem;
     position: absolute;
-}
+  }
 
-.checkbox-container input:checked + .checkmark:after {
+  .checkbox-container input:checked + .checkmark:after {
     content: "";
     position: absolute;
     height: 0.25rem;
@@ -127,26 +139,52 @@ input:focus {
     top: 25%;
     left: 21%;
     transform: rotate(-45deg);
-}
+  }
 
+  .need-help {
+    text-decoration: none;
+    color: #828282;
+    margin-left: 6.6rem;
+    font-size: 0.9rem;
+  }
+
+  .bottom-form img {
+    width: 1.5625rem;
+    margin: 0.625rem 0.625rem -0.4375rem 0;
+  }
+  .login-fb-text {
+    color: #828282;
+    font-size: 0.9rem;
+  }
+
+  .bottom-form {
+      position: absolute;
+      bottom: 0;
+      margin-bottom: 4rem;
+  }
+  .sign-up-text {
+      font-size: 1.1rem;
+      color: #fff;
+      &:hover {
+          text-decoration: underline;
+      }
+  }
 `;
 
 //Button
 
 const Button = styled.button`
-    color: #fff;
-    background: rgba(229,9,20);
-    border: none;
-    outline: none;
-    padding: 0.8rem 1.3rem;
-    border-radius: 0.125rem;
-    font-size: 1rem;
-    text-align: center;
-    box-shadow: 0 1px 0 rgba(0,0,0,0.45);
-    transition: opacity 0.2s ease-in;
-    cursor: pointer;
-    text-decoration: none;
-    margin: 1rem 0;
-
+  color: #fff;
+  background: rgba(229, 9, 20);
+  border: none;
+  outline: none;
+  padding: 0.8rem 1.3rem;
+  border-radius: 0.125rem;
+  font-size: 1rem;
+  text-align: center;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+  transition: opacity 0.2s ease-in;
+  cursor: pointer;
+  text-decoration: none;
+  margin: 1rem 0;
 `;
-
